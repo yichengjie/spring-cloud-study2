0398e655e7db052584d6a31740ef0e9422e8aacf
yichengjie <626659321@qq.com>
2019-06-17 22:39:13 +0800

    旧版本代码提交

---
+++ eureka-consumer/src/main/java/com/yicj/study/cloud2/EurekaConsumerApplication.java
@@ -0,0 +1,22 @@
+package com.yicj.study.cloud2;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaConsumerApplication {
+	
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(EurekaConsumerApplication.class).web(true).run(args);
+	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+}
--- eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableEurekaServer
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EurekaServerApplication {
 	
 	public static void main(String[] args) {
