6afe5f81d39154e589be97f6e2d0e9a27362d9b4
yichengjie <626659321@qq.com>
2019-06-21 22:16:23 +0800

    版本号调整

--- api-gateway/src/main/java/com/yicj/study/cloud2/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/yicj/study/cloud2/ApiGatewayApplication.java
@@ -1,13 +1,25 @@
 package com.yicj.study.cloud2;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-
-@EnableZuulProxy
-@SpringCloudApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+//https://blog.csdn.net/forezp/article/details/83792388
+@SpringBootApplication
+@RestController
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args) ;
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route(p -> p.path("/get")
+			.filters(f -> f.addRequestHeader("Hello", "World"))
+			.uri("http://httpbin.org:80"))
+			.build();
 	}
 }
--- eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-
 //http://blog.didispace.com/spring-cloud-learning/
 @EnableEurekaServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
