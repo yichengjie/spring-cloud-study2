e89eb512edca9316572b69b52e3da5de91f179ea
yichengjie <626659321@qq.com>
2019-06-22 19:01:36 +0800

    网关正常运行

--- api-gateway/src/main/java/com/yicj/study/cloud2/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/yicj/study/cloud2/ApiGatewayApplication.java
@@ -2,24 +2,15 @@ package com.yicj.study.cloud2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 //https://blog.csdn.net/forezp/article/details/83792388
 @SpringBootApplication
-@RestController
+@EnableZuulProxy
+@EnableEurekaClient
+//@EnableAutoConfiguration(exclude = {SecurityAutoConfiguration.class})
 public class ApiGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
-	}
-
-	@Bean
-	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
-		return builder.routes()
-			.route(p -> p.path("/get")
-			.filters(f -> f.addRequestHeader("Hello", "World"))
-			.uri("http://httpbin.org:80"))
-			.build();
 	}
 }
