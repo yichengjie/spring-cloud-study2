1e24804873a21c7762ea98233b935291b821aea5
yichengjie <626659321@qq.com>
2019-06-23 16:04:58 +0800

    重新规划项目端口

--- config-client/src/main/java/com/yicj/study/cloud2/ConfigClientApplication.java
+++ config-client/src/main/java/com/yicj/study/cloud2/ConfigClientApplication.java
@@ -2,10 +2,8 @@ package com.yicj.study.cloud2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ConfigClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigClientApplication.class, args) ;
---
+++ readme.txt
@@ -0,0 +1,7 @@
+1、注册中心eureka-server: 1001
+2、服务提供方eureka-client: 2001
+3、网关中心gateway-zuul: 3001
+4、配置中心config-server-git: 4001
+  4.1配置使用端config-client: 4002
+5、服务消费方
+  5.1 eureka-consumer : 2002
