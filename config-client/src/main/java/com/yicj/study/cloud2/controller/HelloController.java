e9f689b934708ad7ed0c1aa41dcd169d4dbfc004
yichengjie <626659321@qq.com>
2019-06-23 16:47:01 +0800

    增加配置获取失败时重试配置

--- config-client/src/main/java/com/yicj/study/cloud2/controller/HelloController.java
+++ config-client/src/main/java/com/yicj/study/cloud2/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.yicj.study.cloud2.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class HelloController {
 	@Value("${test}")
 	private String test ;
--- eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yicj/study/cloud2/EurekaServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 //http://blog.didispace.com/spring-cloud-learning/
+//https://blog.csdn.net/yeyinglingfeng/article/details/83821059
 @EnableEurekaServer
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EurekaServerApplication {
