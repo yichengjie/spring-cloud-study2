d885b41cd3ccb1f8ab270af9125c7eb92e9cfd51
yichengjie <626659321@qq.com>
2019-06-23 11:54:40 +0800

    配置中心测试2

--- eureka-client/src/main/java/com/yicj/study/cloud2/controller/DcController.java
+++ eureka-client/src/main/java/com/yicj/study/cloud2/controller/DcController.java
@@ -1,6 +1,7 @@
 package com.yicj.study.cloud2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,12 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DcController {
 	@Autowired
-    DiscoveryClient discoveryClient;
+    private DiscoveryClient discoveryClient;
+	@Value("info")
+	private String info ;
 
     @GetMapping("/dc")
     public String dc() {
         String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services);
-        return services;
+        System.out.println(services + "["+info+"]");
+        return services + "["+info+"]";
     }
 }
