7c275aecaef733f03d5f768f2ec1bb4a1d688853
yichengjie <626659321@qq.com>
2019-06-23 15:18:43 +0800

    配置中心的使用

--- eureka-client/src/main/java/com/yicj/study/cloud2/controller/DcController.java
+++ eureka-client/src/main/java/com/yicj/study/cloud2/controller/DcController.java
@@ -1,7 +1,6 @@
 package com.yicj.study.cloud2.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class DcController {
 	@Autowired
     private DiscoveryClient discoveryClient;
-	@Value("${test}")
-	private String test ;
 
     @GetMapping("/dc")
     public String dc() {
         String services = "Services: " + discoveryClient.getServices();
-        System.out.println(services+ " ["+test+"]");
-        return services + " ["+test+"]";
+        return services ;
     }
 }
