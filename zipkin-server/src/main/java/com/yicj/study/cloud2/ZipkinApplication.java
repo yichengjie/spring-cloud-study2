5bb3b277cb78cb903555c365b7c15eeab9a74098
yichengjie <626659321@qq.com>
2019-06-25 22:23:08 +0800

    日志服务器

---
+++ zipkin-server/src/main/java/com/yicj/study/cloud2/ZipkinApplication.java
@@ -0,0 +1,13 @@
+package com.yicj.study.cloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class ZipkinApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
