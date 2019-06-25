bfe3b56fd45686cc4be413a699993ce1b9ecc780
yichengjie <626659321@qq.com>
2019-06-25 14:13:18 +0800

    删除无效配置

---
+++ stream-rabbitmq/src/main/java/com/yicj/study/cloud2/SinkApplication.java
@@ -0,0 +1,16 @@
+package com.yicj.study.cloud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding(Sink.class)
+public class SinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SinkApplication.class, args);
+    }
+
+}
---
+++ stream-rabbitmq/src/main/java/com/yicj/study/cloud2/service/SinkReceiver.java
@@ -0,0 +1,16 @@
+package com.yicj.study.cloud2.service;
+
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
+import lombok.extern.slf4j.Slf4j;
+
+@EnableBinding(Sink.class)
+@Slf4j
+public class  SinkReceiver {
+	
+	@StreamListener(Sink.INPUT)
+    public void receive(Object payload) {
+        log.info("Received: " + payload);
+    }	
+}
